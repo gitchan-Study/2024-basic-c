3c0c54cfcfda4b38af3d8675c8aca67767459fe0
gitchan <eunkeeeee@gmail.com>
2024-03-15 12:29:57 +0900

    feat: 2차원 배열 메모리로 표현

--- 04_two_demensions.c
+++ 04_two_demensions.c
@@ -3,6 +3,8 @@
 
 short countLength(const char data[]);
 
+void printTwoDimension(const short two_dimension[]);
+
 void main() {
     short one_dimension[9] = {4, 6, 9, 8, 7, 2, 5, 1, 3};
     short two_dimension[3][3];
@@ -12,12 +14,16 @@ void main() {
         }
     }
 
+    printTwoDimension(two_dimension);
+
+}
+
+void printTwoDimension(const short two_dimension[]) {
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             short data = two_dimension[i][j];
             printf("%d, %d: %d\n", i, j, data);
         }
     }
-
 }
 
